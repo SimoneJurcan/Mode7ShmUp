c4672734352de2b5628f473dd3982551a7c9fabd
Simone <sjurcan22@student.foi.hr>
2025-08-20 08:47:50 +0200

    added player class and weapon overheat

--- player.py
+++ player.py
@@ -27,3 +27,68 @@ WEAPONS = {
 BASE_SPREAD_VEL = 400.0   
 X_MIN, X_MAX = -800.0, 800.0  
 Y_MIN, Y_MAX = -200.0, 200.0 
+
+class Player:
+    def __init__(self, speed=1200, y_speed=650):
+        self.x = 0.0
+        self.y = 0.0
+        self.speed = speed
+        self.y_speed = y_speed
+
+        self.bullets = []
+        self.bullet_timer = 0.0
+        self.current_weapon = REVOLVER
+
+   
+        self.heat = 0.0
+        self.max_heat = 100.0
+        self.overheated = False
+        self.heat_cooldown_rate = 40.0
+        self.heat_per_shot = 10.0
+        self.overheat_threshold = self.max_heat      #
+        self.recover_threshold  = 40.0             
+
+        self.shield_timer = 0.0
+        self.fire_rate_mult = 1.0
+        self.fire_rate_timer = 0.0
+   
+    def handle_input(self, keys, dt):
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.x -= self.speed * dt
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.x += self.speed * dt
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.y += self.y_speed * dt
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.y -= self.y_speed * dt
+
+        
+        self.x = max(X_MIN, min(X_MAX, self.x))
+        self.y = max(Y_MIN, min(Y_MAX, self.y))
+
+       
+        if keys[pygame.K_1]:
+            self.current_weapon = REVOLVER
+        if keys[pygame.K_2]:
+            self.current_weapon = SHOTGUN
+        if keys[pygame.K_3]:
+            self.current_weapon = MACHINE_GUN
+
+    def update(self, dt, keys):
+        self.handle_input(keys, dt)
+        self.bullet_timer = max(0.0, self.bullet_timer - dt)
+
+    
+        if self.heat > 0.0:
+            self.heat = max(0.0, self.heat - self.heat_cooldown_rate * dt)
+     
+        if self.overheated and self.heat <= self.recover_threshold:
+            self.overheated = False
+
+       
+        if self.shield_timer > 0.0:
+            self.shield_timer = max(0.0, self.shield_timer - dt)
+        if self.fire_rate_timer > 0.0:
+            self.fire_rate_timer = max(0.0, self.fire_rate_timer - dt)
+            if self.fire_rate_timer == 0.0:
+                self.fire_rate_mult = 1.0
